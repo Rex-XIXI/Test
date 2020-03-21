faa7009cd29e6c9a9055a5f708930a066e8ffd57
Rex <870626345@qq.com>
2020-03-21 17:01:20 +0800

    Homework:Temperature conversion

--- app/src/main/java/com/swufe/test/MainActivity.java
+++ app/src/main/java/com/swufe/test/MainActivity.java
@@ -8,16 +8,40 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView out;
+    TextView input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //...
+
+        out=findViewById(R.id.tran_show);
+        input=findViewById(R.id.tran_input);
+
+        Button btn =findViewById(R.id.tran_bot1);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("click","onClick...");
+        Double tem= Double.valueOf(input.getText().toString());
+        out.setText(tem+"°C所对应华氏度为："+convert(tem)+"°F");
+    }
+
+    public double  convert(double tem){
+        return tem*1.8+32;
     }
 }
